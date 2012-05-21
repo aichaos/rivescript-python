7a7804b1659dd390b590c0e4f870e60335f3b6f7
Noah Petherbridge <noah.petherbridge@dreamhost.com>
2012-05-21 14:19:00 -0700

    Fix build.sh to build 32-bit Windows EXE

--- dist.sh
+++ dist.sh
@@ -2,5 +2,5 @@
 
 # Prepare the module for distribution.
 python setup.py bdist_rpm --packager="Noah Petherbridge <root@kirsle.net>" \
-	bdist_wininst \
+	bdist_wininst --plat-name=win32 \
 	bdist_dumb
