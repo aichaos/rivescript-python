6ab4fc7af637976a92846005c4d1d35693e893a0
Noah Petherbridge <noah.petherbridge@zefr.com>
2013-10-04 16:34:25 -0700

    Fix running the module directly
    After rearranging the package structure, running the rivescript module
    directly stopped working due to relative import errors. Fix it so that
    it can be run directly while still working when imported as normal.

--- rivescript/__main__.py
+++ rivescript/__main__.py
@@ -9,9 +9,16 @@ It does nothing more than load the interactive mode of RiveScript."""
 
 __docformat__ = 'plaintext'
 
-from rivescript.interactive import interactive_mode
+# Boilerplate to allow running as script directly.
+# See: http://stackoverflow.com/questions/2943847/nightmare-with-relative-imports-how-does-pep-366-work
+if __name__ == "__main__" and not __package__:
+    import sys, os
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, parent_dir)
+    import rivescript
+    __package__ = str("rivescript")
 
-if __name__ == "__main__":
+    from .interactive import interactive_mode
     interactive_mode()
 
 # vim:expandtab
