e6ff320407dc86019ae4c2a551a1d66e812bc380
hungtu <hungtu@spectrm.de>
2017-07-31 18:33:28 +0200

    Add check for pipe outside brackets and more advanced brackets balance check

--- rivescript/parser.py
+++ rivescript/parser.py
@@ -574,6 +574,14 @@ class Parser(object):
                     angle += 1
                 elif char == '>':
                     angle -= 1
+                elif char == '|':
+                    if parens == 0 and square == 0:   # Pipe outside the alternative and option
+                        return "Pipe | must be within parenthesis brackets or square brackets"
+
+                sum = parens + square + curly + angle   # At each character, not more than 1 bracket opens
+                for special_char_count in [parens, square, curly, angle, sum]:
+                    if special_char_count not in (0, 1):
+                        return "Unbalanced brackets"
 
             # Any mismatches?
             if parens != 0:
